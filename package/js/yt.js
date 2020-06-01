927868aa992bdf3519185c38633ed66e9fff92a4
KevDoy <KevDoy@users.noreply.github.com>
2020-06-01 14:33:19 -0700

    add ability to switch service

---
+++ package/js/yt.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+	var data;
+			$.getJSON("https://ondemand.trenta.io/data/hero.json", function (json) {
+				data = json;
+				var transform = {
+					"<>": "a", "href":"https://www.youtube.com/watch?v=${movieid}", "html": [
+						{
+							"<>": "img", "src": "https://img.youtube.com/vi/${movieid}/maxresdefault.jpg", "class": "img-fluid", "alt":"${movietitle}"
+								
+								}
+							]
+
+				};
+				$(function () {
+
+					//Create the list
+					$('#hero').json2html(data, transform);
+				});
+			});
+
+			$.getJSON("https://ondemand.trenta.io/data/popular.json", function (json) {
+				data = json;
+				var transform = {
+					"<>": "div", "class": "col-3 featured", "html": [
+						{
+							"<>": "a", "href":"https://www.youtube.com/watch?v=${movieid}", "html": [
+								{
+									"<>": "img", "src": "https://img.youtube.com/vi/${movieid}/hqdefault.jpg", "class": "img-fluid", "alt":"${movietitle}" }	
+									]
+								}
+							]
+
+				};
+				$(function () {
+
+					//Create the list
+					$('#featured').json2html(data, transform);
+				});
+			});
+
+
+		
+			$.getJSON("https://ondemand.trenta.io/data/topselling.json", function (json) {
+				data = json;
+				var transform = {
+					"<>": "div", "class": "col-4 topseller", "html": [
+						{
+							"<>": "a", "href":"https://www.youtube.com/watch?v=${movieid}", "html": [
+								{
+									"<>": "img", "src": "https://img.youtube.com/vi/${movieid}/hqdefault.jpg", "class": "img-fluid", "alt":"${movietitle}" }	
+									]
+								}
+							]
+
+				};
+				$(function () {
+
+					//Create the list
+					$('#topselling').json2html(data, transform);
+				});
+			});
